933ec55b466b5adef41374b0d4fa2408d9acd120
sasaki_nozomi <nozomi.sasaki@drecom.co.jp>
2020-05-28 13:05:00 +0900

    モーダル背景が動かないjs調整

--- js/modal-fixed.js
+++ js/modal-fixed.js
@@ -1,13 +1,10 @@
 $(function() {
-	var scrollPos;//topからのスクロール位置
 	$('.modal-syncer').click(function() {
-	  scrollPos = $(window).scrollTop();//topからのスクロール位置を格納
-	  $('body').addClass('fixed').css({ top: -scrollPos });//背景固定
-	  return false;//<a>を無効化
+		$('body').addClass('fixed');//背景固定
+		return false;//<a>を無効化
 	});
-	$('.button-link').click(function() {
-	  $('body').removeClass('fixed').css({ top: 0 });//背景固定を解除
-	  $(window).scrollTop(scrollPos);//元の位置までスクロール
-	  return false;//<a>を無効化
+	$('.button-link').on('click.close-model',function() {
+		$('body').removeClass('fixed');//背景固定を解除
+		return false;//<a>を無効化
 	});
-  });
+});
