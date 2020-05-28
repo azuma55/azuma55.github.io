f7d7fb228fa4debc7b35ecaa23b79cf446f39ce1
hikari_asano <hikari.asano@drecom.co.jp>
2020-05-28 10:35:05 +0900

    recommit

--- js/modal-fixed.js
+++ js/modal-fixed.js
@@ -1,11 +1,13 @@
-$(function(){
-	var scrollPosition;
-	$(".modal-syncer").on("click", function() {
-		scrollPosition = $(window).scrollTop();
-		$('body').addClass('fixed').css({'top': -scrollPosition});
+$(function() {
+	var scrollPos;//topからのスクロール位置
+	$('.modal-syncer').click(function() {
+	  scrollPos = $(window).scrollTop();//topからのスクロール位置を格納
+	  $('body').addClass('fixed').css({ top: -scrollPos });//背景固定
+	  return false;//<a>を無効化
 	});
-	$(".button-link , .button-link-pc").on("click", function() {
-		$('body').removeClass('fixed').css({'top': 0});
-		window.scrollTo( 0 , scrollPosition );
+	$('.button-link').click(function() {
+	  $('body').removeClass('fixed').css({ top: 0 });//背景固定を解除
+	  $(window).scrollTop(scrollPos);//元の位置までスクロール
+	  return false;//<a>を無効化
 	});
-});
+  });
