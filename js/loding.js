0ceda3a492115e3b85e549628ca8bbb1f26fd451
hikari_asano <hikari.asano@drecom.co.jp>
2020-06-15 00:44:14 +0900

    ローディング実験

---
+++ js/loding.js
@@ -0,0 +1,6 @@
+$(function(){
+    var loadingWrap = document.getElementById('loadingWrap');
+    window.addEventListener('load', function() {
+        loadingWrap.classList.add('loadingNone');
+    });
+});
