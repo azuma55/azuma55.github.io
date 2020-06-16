f918d92eceb4bb56f1af01d8f7c82b1cf4064655
hikari_asano <hikari.asano@drecom.co.jp>
2020-06-16 11:15:04 +0900

    loding.js追加

--- js/loding.js
+++ js/loding.js
@@ -1,6 +1,13 @@
 $(function(){
-    var loadingWrap = document.getElementById('loadingWrap');
-    window.addEventListener('load', function() {
-        loadingWrap.classList.add('loadingNone');
-    });
+	var loader = $('.loader-wrap');
+
+	//ページの読み込みが完了したらアニメーションを非表示
+	$(window).on('load',function(){
+		loader.fadeOut();
+	});
+
+	//ページの読み込みが完了してなくても5秒後にアニメーションを非表示にする
+	setTimeout(function(){
+		loader.fadeOut();
+	},5000);
 });
